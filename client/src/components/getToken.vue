58b7d0d61b9b1b2ab0391d50bf2ab750e22ef205
AdhityaRdt <rahmanadhitya2201@gmail.com>
2018-06-07 14:27:09 +0700

    not yet finished

--- client/src/components/getToken.vue
+++ client/src/components/getToken.vue
@@ -4,19 +4,20 @@
     <h3>Get Token</h3>
     <div class="form-group">
       <label for="exampleInputEmail1">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" v-model="email">
       <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Full Name</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter your full name" v-model="name">
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary" @click="addToken">Get Token</button>
   </form>
  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'getToken',
   data () {
@@ -27,20 +28,22 @@ export default {
   },
   methods: {
     addToken: function () {
-          axios.post('http://35.198.212.156/request-token', {
-            email: this.email,
-            name: this.name
-          })
-          .then(response => {
-            if (response.data.uid) {
-              this.success = 'Add token sucess'
-              localStorage.setItem('authorization', response.data.uid)
-            }
-          })
-          .catch(err => {
-            this.error = err
-          })
+      axios.post('http://35.197.135.159/request-token', {
+        email: this.email,
+        name: this.name
+      })
+      .then(response => {
+        // console.log(response)
+        if (response.data.uuid) {
+          console.log('Add token sucess')
+          console.log('This is your token/uuid', response.data.uuid)
+          localStorage.setItem('authorization', response.data.uuid)
         }
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
   }
 }
 </script>
--- client/src/components/postImage.vue
+++ client/src/components/postImage.vue
@@ -1,15 +1,53 @@
 <template>
   <div>
-    <h1> This is post image</h1>
+    <form>
+      <h3>Post an Image</h3>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Select an image</label>
+        <input type="file" class="form-control" id="exampleInputEmail1" @change="uploadHandler">
+        <small id="emailHelp" class="form-text text-muted">Please post an appropriate image</small>
+      </div>
+      <button type="submit" class="btn btn-primary" @click="uploadImage">POST</button>
+    </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'postImage'
+  name: 'postImage',
+  data () {
+    return {
+      image: ''
+    }
+  },
+  methods: {
+    uploadImage: function () {
+      let formData = new FormData()
+      formData.append('file', this.image)
+      axios.post('http://35.197.135.159/image', formData, {
+        'Content-Type': 'multipart/form-data',
+        authorization: localStorage.getItem('authorization')
+      })
+      .then(response => {
+        console.log('upload image sucess')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    },
+    uploadHandler: function (event) {
+      this.image = event.target.files[0]
+    }
+  }
 }
 </script>
 
 <style>
-
+form
+{
+  border:thin black solid;
+  margin:20px;
+  padding:20px;
+}
 </style>
